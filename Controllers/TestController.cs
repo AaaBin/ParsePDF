19df82aa8f493362c11094c03b73889a9d62cbe3
AaaBin <bernie.tseng@tpisoftware.com>
2021-07-22 17:00:37 +0800

    Add TestController, SayHello Method

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+namespace ParsePDF.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public async Task<IActionResult> SayHELLO()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            return Ok("HELLO");
+        }
+    }
+}
